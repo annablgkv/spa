95670b2c4111e8ea3cd5230464a2e850358b0a37
mioe <mioe.boss@gmail.com>
2018-02-24 00:21:53 +0300

    mobiMenu le fin

--- js/main.js
+++ js/main.js
@@ -1,18 +1,30 @@
 // nav logic (start)
 $('nav li').mouseenter(function () {
-  $('.sub-menu', this).show()
-  $('.sub-menu .sub-menu', this).hide()
+  $('nav .sub-menu', this).show()
+  $('nav .sub-menu .sub-menu', this).hide()
 })
 
 $('nav li').mouseleave(function () {
-  $('.sub-menu', this).hide()
+  $('nav .sub-menu', this).hide()
 })
 
-$('.sub-menu li').mouseenter(function () {
-  $('.sub-menu', this).show()
+$('nav .sub-menu li').mouseenter(function () {
+  $('nav .sub-menu', this).show()
 })
 
-$('.sub-menu li').mouseleave(function () {
-  $('.sub-menu', this).hide()
+$('nav .sub-menu li').mouseleave(function () {
+  $('nav .sub-menu', this).hide()
+})
+
+$('#btnMobiMenu').click(function () {
+  $('#mobiMenu').css({
+    "right": "0px"
+  })
+})
+
+$("#btnCloseMobiMenu").click(function () {
+  $('#mobiMenu').css({
+    "right": "-300px"
+  })
 })
 // nav logic (end)
