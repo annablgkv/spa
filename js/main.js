48baf00af100d2a61afba3a4bc62bb14cd8015d2
mioe <mioe.boss@gmail.com>
2018-02-25 16:21:20 +0300

    xl & lg & md le fin

--- js/main.js
+++ js/main.js
@@ -64,4 +64,94 @@ $('#mainSlider').click(function () {
   }
 
 })
-// main slider logic(start)
+// main slider logic(end)
+
+
+// blockServices dataset (start)
+var dataServices = [
+  {
+    name: "Oil massages",
+    price: "50,80",
+    desc: "Lorem ipsum dolor sit amet ",
+    classTag: "New1",
+    tag: "new"
+  },
+  {
+    name: "Nails art",
+    price: "29,80",
+    desc: "Lorem ipsum dolor sit amet ",
+    classTag: "New2",
+    tag: "new"
+  },
+  {
+    name: "Face scrubs",
+    price: "70,30",
+    desc: "Lorem ipsum dolor sit amet ",
+    classTag: "",
+    tag: "1 hour"
+  },
+  {
+    name: "Back massage",
+    price: "50,80",
+    desc: "Lorem ipsum dolor sit amet ",
+    classTag: "",
+    tag: "1 hour"
+  },
+  {
+    name: "Sauna relax",
+    price: "23,45",
+    desc: "Lorem ipsum dolor sit amet ",
+    classTag: "New2",
+    tag: "new"
+  },
+  {
+    name: "Aroma therapy",
+    price: "50,80",
+    desc: "Lorem ipsum dolor sit amet ",
+    classTag: "New1",
+    tag: "new"
+  },
+  {
+    name: "Pedicure",
+    price: "32,00",
+    desc: "Lorem ipsum dolor sit amet ",
+    classTag: "",
+    tag: "1 hour"
+  },
+  {
+    name: "Mug masks",
+    price: "43,00",
+    desc: "Lorem ipsum dolor sit amet ",
+    classTag: "New1",
+    tag: "new"
+  },
+]
+
+
+for (var i=0; i < 4; i++) {
+  document.getElementById('itemBlockServices1').innerHTML += ''+
+    '<div class="itemBlockServies">'+
+      '<div class="procLine"></div>'+
+      '<div class="procName"><p>'+ dataServices[i].name +'</p></div>'+
+      '<div class="procPrice"><p>$ '+ dataServices[i].price +'</p></div>'+
+      '<div class="procDesc"><p>'+ dataServices[i].desc +'</p></div>'+
+      '<div class="procTag' + dataServices[i].classTag + ' dontTouchMe"><p>'+ dataServices[i].tag +'</p></div>'+
+    '</div>'
+}
+
+for (var i = 4; i < 8; i++) {
+  document.getElementById('itemBlockServices2').innerHTML += '' +
+    '<div class="itemBlockServies">' +
+    '<div class="procLine"></div>' +
+    '<div class="procName"><p>' + dataServices[i].name + '</p></div>' +
+    '<div class="procPrice"><p>$ ' + dataServices[i].price + '</p></div>' +
+    '<div class="procDesc"><p>' + dataServices[i].desc + '</p></div>' +
+    '<div class="procTag' + dataServices[i].classTag + ' dontTouchMe"><p>' + dataServices[i].tag + '</p></div>' +
+    '</div>'
+}
+
+
+
+
+// blockServices dataset (end)
+
